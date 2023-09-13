c7b383969e715a4f657a73e03a35182159719b10
Hallotomhier <tomwinkel2005@gmail.com>
2023-09-13 14:19:51 +0200

    KanonToren

---
+++ TowerDefense Periode5/Assets/TomTest/DetectShip.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectShip : MonoBehaviour
+{
+    private bool isDetect;
+    private Vector3 center;
+    public int range;
+    public GameObject ball;
+   
+    
+
+    void Start()
+    {
+        center = gameObject.transform.position;
+    }
+
+    
+    void Update()
+    {
+       
+        Collider[] hitColliders = Physics.OverlapSphere(center , range);
+        foreach (var hitCollider in hitColliders) 
+        {
+            
+            if (hitCollider != gameObject) 
+            {
+                
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(center, range);
+    }
+}
